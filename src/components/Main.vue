b6beeffebcae9dbf028517ae52fe183a2f97af68
adventure-star <Boristolstyakov@protonmail.com>
2021-01-19 09:13:05 +0700

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="text-large text-red-500">
+    Main
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props: {
+        msg: String
+    },
+    data() {
+
+        return {
+            choice: 'OK',
+        }
+
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import App from './App.vue'
+import CoreuiVue from '@coreui/vue'
+
+import './assets/css/talwindcss.css';
+
+Vue.config.productionTip = false
+
+Vue.use(CoreuiVue);
+
+Vue.use({
+  install: function(Vue){
+      Vue.prototype.$jQuery = require('jquery'); // you'll have this.$jQuery anywhere in your vue project
+      }
+  });
+
+new Vue({
+
+  render: h => h(App),
+  
+}).$mount('#app')
+
+
+// new Vue({
+//   el: '#app',
+//   // router,
+//   // store,
+//   // icons,
+//   // template: '<App/>',
+//   components: {
+//     'testtemplate' : '<App/>'
+//   }
+// })
